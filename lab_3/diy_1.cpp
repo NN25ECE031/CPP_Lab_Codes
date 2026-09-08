e48bfbbcaeb449f59efb3a67215d79ac8aa8abce
Aparna Prabhu <nn25ece031@nmamit.in>
2026-09-08 14:49:19 +0530

    Update diy_1.cpp

--- lab_3/diy_1.cpp
+++ lab_3/diy_1.cpp
@@ -5,20 +5,19 @@ class Rectangle {
 private:
     double length;
     double width;
-
 public:
     void setLength(double l) {
         if (l >= 0)
             length = l;
         else
-            cout << "Invalid length!" << endl;
+            cout << "Invalid length" << endl;
     }
 
     void setWidth(double w) {
         if (w >= 0)
             width = w;
         else
-            cout << "Invalid width!" << endl;
+            cout << "Invalid width" << endl;
     }
 
     double area() {
